f99d338790ebc8851cac52d1cda9dc948827a103
Alejandro Hernández Hernández <alejhern@student.42barcelona.com>
2024-08-16 23:36:29 +0200

    cambio de nombre

--- ft_printf.h
+++
@@ -1,8 +0,0 @@
-# include <stdio.h>
-# include <stdarg.h>
-# include "libft/libft.h"
-
-int	ft_printpointer(void *ptr);
-int ft_printnbr(int nb, int base, unsigned int isunsigned, unsigned int bl_mayus);
-int ft_printstr(char *str, unsigned int bl_mayus);
-int	ft_printchar(char c);
